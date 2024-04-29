d6e42a99c220bd422aba2d49b98c3eb54bf4d704
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-04-29 16:24:02 -0700

    web server setup using puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,54 +1,29 @@
 # Configure Nginx with custom HTTP response header and custom pages in Puppet
 
-package { 'nginx':
-  ensure => installed,
+exec { 'update apps':
+        command => '/usr/bin/apt-get update',
 }
 
-file { '/etc/nginx/html':
-  ensure => 'directory',
-  require => Package['nginx'],
+package { 'install nginx':
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-file { '/etc/nginx/html/index.html':
-  ensure  => 'present',
-  content => 'Hello World!',
-  require => File['/etc/nginx/html'],
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
-file { '/etc/nginx/html/404.html':
-  ensure  => 'present',
-  content => "Ceci n'est pas une page",
-  require => File['/etc/nginx/html'],
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://github.com/MedBens02 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'present',
-  content => @(EOF)
-    server {
-      listen 80;
-      listen [::]:80 default_server;
-      root   /etc/nginx/html;
-      index  index.html index.htm;
-      add_header X-Served-By $::hostname;
-
-      location /redirect_me {
-        return 301 https://github.com/MedBens02;
-      }
-
-      error_page 404 /404.html;
-      location /404 {
-        root /etc/nginx/html;
-        internal;
-      }
-    }
-    | EOF
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+exec {'HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
-service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  require    => File['/etc/nginx/sites-available/default'],
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
-
