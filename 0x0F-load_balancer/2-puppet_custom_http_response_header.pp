bb0a948805257f648b1b79f73c2d794ff8017c35
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-04-29 16:34:23 -0700

    setup using puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,29 +1,55 @@
 # Configure Nginx with custom HTTP response header and custom pages in Puppet
 
-exec { 'update apps':
-        command => '/usr/bin/apt-get update',
+# Ensure the package list is always updated before attempting installation
+exec { 'update-system':
+  command     => '/usr/bin/apt-get update',
+  path        => ['/bin', '/usr/bin'],
+  logoutput   => true,
+  unless      => "apt-cache policy | grep 'nginx'",
+  before      => Package['nginx'],
 }
 
-package { 'install nginx':
-	ensure => 'installed',
-	require => Exec['update system']
+# Install Nginx
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['update-system'],
 }
 
-file {'/var/www/html/index.html':
-	content => 'Hello World!'
+# Configure the main site
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('nginx/default.erb'),
+  require => Package['nginx'],
+  notify  => Service['nginx'],
 }
 
-exec {'redirect_me':
-	command => 'sed -i "24i\	rewrite ^/redirect_me https://github.com/MedBens02 permanent;" /etc/nginx/sites-available/default',
-	provider => 'shell'
+# Ensure Nginx is running and enabled to start at boot
+service { 'nginx':
+  ensure => running,
+  enable => true,
 }
 
-exec {'HTTP header':
-	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
-	provider => 'shell'
-}
+# Template for /etc/nginx/sites-available/default
+file {'nginx/default.erb':
+  path    => "/etc/puppet/modules/nginx/templates/default.erb",
+  content => @(EOF)
+    server {
+      listen 80;
+      listen [::]:80 default_server;
+      root /var/www/html;
+      index index.html index.htm;
 
-service {'nginx':
-	ensure => running,
-	require => Package['nginx']
+      add_header X-Served-By $::hostname;
+
+      location /redirect_me {
+        rewrite ^/redirect_me https://github.com/MedBens02 permanent;
+      }
+
+      error_page 404 /404.html;
+      location = /404.html {
+        root /var/www/html;
+        internal;
+      }
+    }
+    | EOF
 }
