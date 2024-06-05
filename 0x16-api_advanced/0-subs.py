9a3c6d4fd5d347059f78b5c74e69638f5671af96
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-06-04 18:28:50 -0700

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     '''Function that queries the Reddit API'''
     resp = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        "https://www.reddit.com/r/{}/about/.json".format(subreddit),
         headers={
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
                            AppleWebKit/537.36 (KHTML, like Gecko) \
@@ -14,8 +14,8 @@ def number_of_subscribers(subreddit):
         },
         allow_redirects=False
     )
-    if resp.status_code == 200:
+    if resp.status_code != 200:
+        return 0
+    else:
         results = resp.json().get("data")
         return results.get("subscribers")
-    else:
-        return 0
